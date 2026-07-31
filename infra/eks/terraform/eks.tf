fd897c4b04ec095090499be69dea3bac33e096ef
trake25 <enriquedelapenajr@gmail.com>
2026-08-01 07:11:51 +0800

    DevOps EKS v0.09

--- infra/eks/terraform/eks.tf
+++ infra/eks/terraform/eks.tf
@@ -17,6 +17,11 @@ resource "aws_eks_cluster" "main" {
     public_access_cidrs     = ["0.0.0.0/0"]
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   enabled_cluster_log_types = var.eks_cluster_log_types
 
   depends_on = [
